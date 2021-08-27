60fe4c60bbf49cb41b9b0ae7cdd086bb8cd59509
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-08-27 22:32:56 +0300

    first commit

---
+++ scores.js
@@ -0,0 +1,37 @@
+'usestrict';
+let scores = [60, 50, 60, 58, 54, 54,
+    58, 50, 52, 54, 48, 69,
+    34, 55, 51, 52, 44, 51,
+    69, 64, 66, 55, 52, 61,
+    46, 31, 57, 52, 44, 18,
+    41, 53, 55, 61, 51, 44];
+
+let max = 0;
+let maxNum = [];
+
+for (let i = 0; i < scores.length; i++) {
+    if (scores[i] > max) {
+        max = scores[i];
+    }
+
+    console.log(`Bubble solution #${i} score: ${scores[i]}`);
+}
+
+for (let i = 0; i < scores.length; i++) {
+    if (scores[i] === max) {
+        maxNum.push(` #${i}`);
+    }
+}
+
+console.log(`Bubbles tests: ${scores.length}`);
+console.log(`Highest bubble score: ${max}`);
+console.log(`Solutions with highest score:${maxNum}`);
+
+/*
+Bubble solution #0 score: 60
+Bubble solution #1 score: 50
+Bubble solution #2 score: 60
+Bubbles tests: 36
+Highest bubble score: 69
+Solutions with highest score: #11, #18
+*/
