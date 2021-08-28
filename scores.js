7d6e96e203c4f9746263dad7c78f029e01c3d748
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-08-28 22:59:20 +0300

    refactoring: rename variables

--- scores.js
+++ scores.js
@@ -5,34 +5,34 @@ let scores = [60, 50, 60, 58, 54, 54,
     69, 64, 66, 55, 52, 61,
     46, 31, 57, 52, 44, 18,
     41, 53, 55, 61, 51, 44];
-
 let output;
-let max = 0;
-let maxNum = [];
+let highScore = 0;
+let bestSolutions = [];
 
 for (let i = 0; i < scores.length; i++) {
-    if (scores[i] > max) {
-        max = scores[i];
-    }
-
     output = `Bubble solution #${i} score: ${scores[i]}`
     console.log(output);
+
+    if (scores[i] > highScore) {
+        highScore = scores[i];
+    }
 }
 
 for (let i = 0; i < scores.length; i++) {
-    if (scores[i] === max) {
-        maxNum.push(` #${i}`);
+    if (scores[i] === highScore) {
+        bestSolutions.push(` #${i}`);
     }
 }
 
 console.log(`Bubbles tests: ${scores.length}`);
-console.log(`Highest bubble score: ${max}`);
-console.log(`Solutions with highest score:${maxNum}`);
+console.log(`Highest bubble score: ${highScore}`);
+console.log(`Solutions with highest score:${bestSolutions}`);
 
 /*
 Bubble solution #0 score: 60
 Bubble solution #1 score: 50
 Bubble solution #2 score: 60
+.....
 Bubbles tests: 36
 Highest bubble score: 69
 Solutions with highest score: #11, #18
